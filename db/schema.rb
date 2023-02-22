b7b5569a5a81ecea24627fbe9a7c53b8bda8ddbe
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-22 16:08:53 +0100

    INTEGRATE SKILLS MODEL VIA MODEL GENERATOR

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_22_095920) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_22_143323) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.integer "percent_utilized"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
