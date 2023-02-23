3c7ca8891490488421bab34b8c9e29ab0df3c7e6
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-23 08:24:09 +0100

    CREATE SEED FILES FOR SAMPLE DATA

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+10.times do |blog|
+    Blog.create!(
+        title: "My Blog Post #{blog}",
+        body: "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, 
+        nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate 
+        velit esse quam nihil molestiae consequatur, 
+        vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+    )
+end
+
+5.times do |skill|
+    Skill.create!(
+        title: "Rails #{skill}",
+        percent_utilized: 25
+    )
+end
+
+9.times do |portfolio_item|
+    Portfolio.create!(
+        title: "Portfolio title #{portfolio_item}",
+        subtitle: "My great portfolio",
+        body: "Ut enim ad minima veniam, quis nostrum exercitationem ullam 
+        corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur,
+         vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+        main_image: "http://via.placeholder.com/600x400",
+        thumb_image: "http://via.placeholder.com/350x200"
+    )
+end
